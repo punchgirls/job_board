f8384d8e04d6ea94f3afcea0e9e3a0efd96d9983
maynkj <mayn.kjaer@gmail.com>
2013-07-22 15:37:27 -0300

    Refactor company_signup.rb.

--- models/company_signup.rb
+++ models/company_signup.rb
@@ -1,13 +1,15 @@
 class CompanySignup < Scrivener
- attr_accessor :company_name, :company_email, :company_url, :password, :password_confirmation
+  attr_accessor :company_name, :company_email, :company_url, :password, :password_confirmation
 
- def validate
-   assert_email(:company_email) && assert(Company.fetch(company_email).nil?, [:company_email, :not_unique])
+  def validate
+    if assert_email(:company_email)
+      assert(Company.fetch(company_email).nil?, [:company_email, :not_unique])
+    end
 
-   assert_present :company_name
-   assert_url :company_url
+    assert_present :company_name
+    assert_url :company_url
 
-   assert_present :password
-   assert password == password_confirmation, [:password, :not_confirmed]
- end
+    assert_present :password
+    assert password == password_confirmation, [:password, :not_confirmed]
+  end
 end
