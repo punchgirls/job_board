10ed871733521e69cdf102dc6004d82bb5c13a2b
Cecilia Rivero <ceciliarc@yahoo.com>
2013-07-23 16:42:30 -0300

    Change res.redirect to render(...) in guests.rb.

--- routes/guests.rb
+++ routes/guests.rb
@@ -10,7 +10,7 @@ class Guests < Cuba
           authenticate(company)
 
           session[:success] = "You have successfully signed up!"
-          res.redirect "/dashboard"
+          render("company_dashboard", title: "Company dashboard")
         elsif Company.with(:email, company_signup.email)
           session[:error] = "This e-mail is already registered"
           render("company_signup", title: "Company signup")
