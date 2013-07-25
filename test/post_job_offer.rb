5cee59e2bf7e93b8eee24958b3d350f394ee9e38
Cecilia Rivero <ceciliarc@yahoo.com>
2013-07-25 13:34:30 -0300

    Add edit post test to post_job_offer.rb.

--- test/post_job_offer.rb
+++ test/post_job_offer.rb
@@ -82,4 +82,30 @@ scope do
 
     assert last_response.body["Post successfully removed!"]
   end
+
+  test "should inform User of post edited" do
+    get "/company_login"
+
+    post "/company_login", { email: "punchgirls@mail.com",
+          password: "1234"}
+
+    get "/dashboard"
+
+    get "/post_job_offer"
+
+    post "/post_job_offer", { post: {  title: "Ruby developer",
+          description: "Ruby oracle needed!" }}
+
+    get "/dashboard"
+
+    get "/edit_post/1"
+
+    post "/edit_post/1", { post: {  title: "Ruby master",
+          description: "Ruby oracle needed!" }}
+
+    get "/dashboard"
+
+    assert last_response.body["Post successfully edited!"]
+    assert last_response.body["Ruby master"]
+  end
 end
