42c94a0f775f5510e0fbd930523136360fa0f2ac
Cecilia Rivero <ceciliarc@yahoo.com>
2013-07-25 17:02:51 -0300

    Add display company account test.

---
+++ test/company_account.rb
@@ -0,0 +1,24 @@
+require "cuba/test"
+require_relative "../app"
+
+prepare do
+  Ohm.flush
+
+  Company.create({ name: "Punchgirls",
+          email: "punchgirls@mail.com",
+          url: "http://www.punchgirls.com",
+          password: "1234" })
+end
+
+scope do
+  test "should display company account" do
+    get "/company_login"
+
+    post "/company_login", { email: "punchgirls@mail.com",
+          password: "1234"}
+
+    get "/company_account"
+
+    assert last_response.body["punchgirls@mail.com"]
+  end
+end
