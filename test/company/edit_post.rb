3d23694ba5d4daeb1a6356d45d9b0e64ada67ff6
maynkj <mayn.kjaer@gmail.com>
2013-09-03 16:54:41 -0300

    Add test for edit post.

---
+++ test/company/edit_post.rb
@@ -0,0 +1,122 @@
+require "cuba/test"
+require_relative "../../app"
+
+prepare do
+  Ohm.flush
+
+  Company.create({ name: "Punchgirls",
+          email: "punchgirls@mail.com",
+          url: "http://www.punchgirls.com",
+          password: "12345678" })
+
+  time = Time.new.to_i
+
+  Post.create({ date: time,
+    expiration_date: time + (30 * 24 * 60 * 60),
+    tags: "",
+    title: "Ruby developer",
+    description: "Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit. Morbi condimentum,
+    odio at fringilla commodo, tellus nisi bibendum
+    ",
+    company_id: "1" })
+end
+
+scope do
+  test "should inform of missing tags" do
+      post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: [],
+          title: "Ruby master",
+          description: "Ruby oracle needed!" }}
+
+    assert last_response.body["At least one skill is required"]
+  end
+
+  test "should inform of missing title" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
+          title: "",
+          description: "Ruby oracle needed!" }}
+
+    assert last_response.body["Title is required"]
+  end
+
+  test "should inform of title out of range" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
+          title: "Ruby oracle needed! Ruby oracle needed!
+            Ruby oracle needed! Ruby oracle needed!
+            Ruby oracle needed! Ruby oracle needed!
+            Ruby oracle needed! Ruby oracle needed!",
+          description: "Ruby oracle needed!" }}
+
+    assert last_response.body["Title should not exceed 80 characters"]
+  end
+
+  test "should inform of missing description" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
+          title: "Ruby master",
+          description: "" }}
+
+    assert last_response.body["Description is required"]
+  end
+
+  test "should inform of description out of range" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
+          title: "Ruby master",
+          description: "Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Morbi condimentum,
+            odio at fringilla commodo, tellus nisi bibendum
+            tellus, sit amet posuere tortor nunc vitae quam.
+            Pellentesque lacus quam, hendrerit vel orci eu,
+            varius sodales urna. Suspendisse faucibus
+            vehicula iaculis. Vivamus vulputate est tellus,
+            ac semper orci ullamcorper ut. Aliquam erat volutpat.
+            Cras sollicitudin, velit vel sollicitudin molestie,
+            est purus suscipit felis, sit amet sagittis nisl
+            lorem quis nisl. Aliquam posuere sem et ligula
+            pulvinar auctor. Sed vel tortor nunc. In hac
+            habitasse platea. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Morbi condimentum,
+            odio at fringilla commodo, tellus nisi bibendum
+            tellus, sit amet posuere tortor nunc vitae quam.
+            Pellentesque lacus quam, hendrerit vel orci eu,
+            varius sodales urna. Suspendisse faucibus
+            vehicula iaculis. Vivamus vulputate est tellus,
+            ac semper orci ullamcorper ut. Aliquam erat volutpat.
+            Cras sollicitudin, velit vel sollicitudin molestie,
+            est purus suscipit felis, sit amet sagittis nisl
+            lorem quis nisl. Aliquam posuere sem et ligula
+            pulvinar auctor. Sed vel tortor nunc. In hac
+            habitasse platea." }}
+
+    assert last_response.body["Description should not exceed 600 characters"]
+  end
+
+  test "should inform of successful update" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
+          title: "Ruby master",
+          description: "Ruby oracle needed!" }}
+
+    follow_redirect!
+
+    assert last_response.body["Post successfully edited!"]
+
+    assert last_response.body["Ruby master"]
+  end
+end
--- test/company/new_post.rb
+++ test/company/new_post.rb
@@ -11,14 +11,37 @@ prepare do
 end
 
 scope do
-  test "should inform User in case of incomplete fields" do
+  test "should inform User of no tags" do
     post "/login", { email: "punchgirls@mail.com",
           password: "12345678" }
 
-    post "/post/new", { post: {  title: "",
+    post "/post/new", { post: { tags: [],
+          title: "Ruby developer",
+          description: "Ruby oracle needed!" }}
+
+    assert last_response.body["At least one skill is required"]
+  end
+
+  test "should inform User of missing title" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/new", { post: { tags: ["Ruby", "Cuba"],
+          title: "",
           description: "Ruby oracle needed!" }}
 
     assert last_response.body["Title is required"]
+  end
+
+  test "should inform User of missing description" do
+    post "/login", { email: "punchgirls@mail.com",
+          password: "12345678" }
+
+    post "/post/new", { post: { tags: ["Ruby", "Cuba"],
+          title: "Ruby developer",
+          description: "" }}
+
+    assert last_response.body["Description is required"]
   end
 
   test "should successfully post a new job offer" do
@@ -60,24 +83,5 @@ scope do
     follow_redirect!
 
     assert last_response.body["Post successfully removed!"]
-  end
-
-  test "should inform User of job edited" do
-    post "/login", { email: "punchgirls@mail.com",
-          password: "12345678" }
-
-    post "/post/new", { post: {  tags: ["Ruby", "Cuba"],
-          title: "Ruby developer",
-          description: "Ruby oracle needed!" }}
-
-    post "/post/edit/1", { post: {  tags: ["Ruby", "Cuba"],
-          title: "Ruby master",
-          description: "Ruby oracle needed!" }}
-
-    follow_redirect!
-
-    assert last_response.body["Post successfully edited!"]
-
-    assert last_response.body["Ruby master"]
   end
 end
