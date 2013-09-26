b5e88e1d69357aa8d2e28c02220d5189cb6080eb
maynkj <mayn.kjaer@gmail.com>
2013-09-26 16:03:42 -0300

    Add AJAX to remove post feature.

--- public/js/posts.js
+++ public/js/posts.js
@@ -22,3 +22,25 @@ function favorite (id) {
 
   request.send();
 }
+
+function removePost (id) {
+  var post = document.getElementById(id);
+  var posts = document.getElementById("posts-list");
+  var url = "post/remove/" + id;
+
+  if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+  } else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  request.open("POST", url);
+
+  request.onreadystatechange = function() {
+    if ((request.readyState===4) && (request.status===200)) {
+      posts.removeChild(post);
+    }
+  };
+
+  request.send();
+}
