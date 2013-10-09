a3937646d7156fa9ed5fd692a8927b28012f5c6d
Cecilia Rivero <ceciliarc@yahoo.com>
2013-10-09 12:54:50 -0300

    Add countdown characters for messages when applying.

--- public/js/limit_description.js
+++ public/js/limit_description.js
@@ -7,6 +7,15 @@ function updateCountdown() {
     countdown.innerHTML = remaining + " characters remaining.";
 }
 
+function updateCountdownMsg(id) {
+    var messageTxt = document.getElementById("messageTxt_" + id);
+    var countdownMsg = document.getElementById("countdownMsg_" + id);
+
+    // 200 is the max message length
+    var remaining = 200 - messageTxt.value.length;
+    countdownMsg.innerHTML = remaining + " characters remaining.";
+}
+
 description.onkeyup = function() {
     updateCountdown();
 };
