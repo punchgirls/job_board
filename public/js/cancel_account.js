757dcf7d539296f1396d2e6c8048d915230f6ee4
Mayn Ektvedt Kjær <mayn.kjaer@gmail.com>
2014-01-23 11:06:40 -0300

    Add confirm dialog on cancel subscription.

--- public/js/cancel_account.js
+++ public/js/cancel_account.js
@@ -1,11 +1,23 @@
 var msg = "Are you sure you want to cancel your account?";
+var cancelMsg = "Are you sure you want to cancel your subscription?";
 var link = document.getElementById('delete');
+var cancelLink = document.getElementById('cancel');
 
 link.onclick = function () {
   var cancel = confirm(msg);
 
   if (cancel) {
     window.location = '/delete';
+  }
+
+  return false;
+};
+
+cancelLink.onclick = function () {
+  var cancel = confirm(cancelMsg);
+
+  if (cancel) {
+    window.location = '/cancel_subscription';
   }
 
   return false;
