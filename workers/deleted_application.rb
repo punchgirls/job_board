f0fbd53b5caf0b2ac6e090f5b0aceab176b1504f
Mayn Ektvedt Kjær <mayn.kjaer@gmail.com>
2014-02-23 19:20:18 -0300

    Revert "Check if application is not null before deleting it."
    This reverts commit 6517ed35a92af35950721d8ed9261115b2a0283c.

--- workers/deleted_application.rb
+++ workers/deleted_application.rb
@@ -3,15 +3,13 @@ require_relative "../app"
 Ost[:deleted_application].each do |id|
   application = Application[id]
 
-  if application
-    text = Mailer.render("deleted_application", { application: application,
-    post: application.post })
+  text = Mailer.render("deleted_application", { application: application,
+  post: application.post })
 
-    Malone.deliver(
-      to: application.post.company.email,
-      subject: "[Job Board] Application has been removed by applicant",
-      text: text)
+  Malone.deliver(
+    to: application.post.company.email,
+    subject: "[Job Board] Application has been removed by applicant",
+    text: text)
 
-    application.delete
-  end
+  application.delete
 end
