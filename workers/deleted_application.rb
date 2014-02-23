6517ed35a92af35950721d8ed9261115b2a0283c
Mayn Ektvedt Kjær <mayn.kjaer@gmail.com>
2014-02-23 19:09:31 -0300

    Check if application is not null before deleting it.

--- workers/deleted_application.rb
+++ workers/deleted_application.rb
@@ -3,13 +3,15 @@ require_relative "../app"
 Ost[:deleted_application].each do |id|
   application = Application[id]
 
-  text = Mailer.render("deleted_application", { application: application,
-  post: application.post })
+  if application
+    text = Mailer.render("deleted_application", { application: application,
+    post: application.post })
 
-  Malone.deliver(
-    to: application.post.company.email,
-    subject: "[Job Board] Application has been removed by applicant",
-    text: text)
+    Malone.deliver(
+      to: application.post.company.email,
+      subject: "[Job Board] Application has been removed by applicant",
+      text: text)
 
-  application.delete
+    application.delete
+  end
 end
