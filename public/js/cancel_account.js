07ea1862d50643b09017da1d07bd02aa4222ab25
Mayn Ektvedt Kjær <mayn.kjaer@gmail.com>
2014-03-03 17:35:43 -0300

    Fix cancel subscription and delete account liks for company.

--- public/js/cancel_account.js
+++ public/js/cancel_account.js
@@ -3,27 +3,13 @@ var cancelLink = document.getElementById('cancel');
 
 if (link) {
   link.onclick = function () {
-    var cancel = confirm(msg);
-    var msg = "Are you sure you want to cancel your account?";
-
-    if (cancel) {
-      window.location = '/delete';
-    }
-
-    return false;
+    return confirm("Are you sure you want to cancel your account?");
   };
 }
 
 if (cancelLink) {
   cancelLink.onclick = function () {
-    var cancel = confirm(cancelMsg);
-    var cancelMsg = "Are you sure you want to cancel your subscription?";
-
-    if (cancel) {
-      window.location = '/cancel_subscription';
-    }
-
-    return false;
+    return confirm("Are you sure you want to cancel your subscription?");
   };
 }
 
