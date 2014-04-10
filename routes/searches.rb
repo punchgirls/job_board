eef76a71ac195eaa3cde6a62d4336977e5f64c19
Cecilia Rivero <ceciliarc@yahoo.com>
2014-04-10 19:22:06 -0300

    Add posts locations in Search results page.

--- routes/searches.rb
+++ routes/searches.rb
@@ -2,38 +2,63 @@ class Searches < Cuba
   define do
     on get, param("query") do |query|
       on query.include?("All posts") do
+        posts = Post.active.sort_by(:date, order: "ALPHA DESC")
+
+        locations = Location.count(posts)
+
         render("search", title: "Search",
-          posts: Post.active.sort_by(:date, order: "ALPHA DESC"),
+          posts: posts, locations: locations,
           search: true, profile: true, query: "All posts", all_posts_link: true)
       end
 
       on default do
         posts = Search.posts(query)
 
+        locations = Location.count(posts)
+
         render("search", title: "Search",
           posts: posts.sort_by(:date, order: "ALPHA DESC"),
-          search: true, profile: true, query: query, all_posts_link: true)
+            locations: locations, search: true,
+            profile: true, query: query, all_posts_link: true)
       end
     end
 
     on param "company_id" do |id|
+      locations = Location.count(Post.all)
+
       render("search", title: "Search",
         posts: Post.active.find(company_id: id).sort_by(:date, order: "ALPHA DESC"),
-        search: true, profile: true, all_posts_link: true)
+        locations: locations, search: true, profile: true, all_posts_link: true)
     end
 
     on param "post_id" do |id|
       post = Post[id]
 
+      locations = Location.count(Post.all)
+
       on post && post.published? do
-        render("search", title: "Search",
+        render("search", title: "Search", locations: locations,
           posts: [post], search: true, profile: true, all_posts_link: true)
       end
 
       on default do
-        render("search", title: "Search",
+        render("search", title: "Search", locations: locations,
           posts: nil, search: true, profile: true, all_posts_link: true)
       end
+    end
+
+    on param "location" do |location|
+      if location == "Work from anywhere"
+        posts = Post.active.find(remote: "true").sort_by(:date, order: "ALPHA DESC")
+      else
+        posts = Post.active.find(location: location).sort_by(:date, order: "ALPHA DESC")
+      end
+
+      locations = Location.count(Post.all)
+
+      render("search", title: "Search",
+        posts: posts, locations: locations,
+        search: true, profile: true, all_posts_link: true)
     end
 
     on default do
